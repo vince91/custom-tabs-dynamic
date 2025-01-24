5ed9df3f6d00e9aa7e528ea82abb52d4a3b8d878
vince91 <6903139+vince91@users.noreply.github.com>
2025-01-24 17:10:14 +0100

    add custom tabs; remove unused files

---
+++ app/[slug].tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function DefaultScreen() {
+  return <Text>Default Screen</Text>;
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import {TabList, Tabs, TabSlot, TabTrigger} from 'expo-router/ui';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Tabs>
+      <TabSlot/>
+      <TabList>
+        <TabTrigger name="dynamic page 1" href="/test1"/>
+        <TabTrigger name="dynamic page 2" href="/test2"/>
+      </TabList>
+    </Tabs>
   );
 }
